066a36d16c9f140d00125da4cf44bb62c19d1c9d
annakarolinv <karolin.vossbeck@gmail.com>
2021-03-25 16:16:16 +0100

    openstreetmap

--- nz/main.js
+++ nz/main.js
@@ -1,5 +1,9 @@
-console.log("hello world");
-console.log(L);
-
-const map = L.map("map");
-console.log(document.querySelector("#map"));
+const map = L.map("map", {
+    center: [-45.316667, 166.983333],
+    zoom: 15,
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
+});
+// Objekt {} und Liste & Array [] erstellt 
+console.log(document.querySelector("#map")); /* Zugreifen auf ein Element aus einem bestimmten Script */
