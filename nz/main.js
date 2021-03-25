862bf4c31a6d4bb4ff0df25273dd27acdd3b6291
annakarolinv <karolin.vossbeck@gmail.com>
2021-03-25 17:44:02 +0100

    route & popups

--- nz/main.js
+++ nz/main.js
@@ -10,18 +10,34 @@ let stop = {
 console.log(stop.name);
 
 const map = L.map("map", {
-    center: [stop.lat, stop.lng],
-    zoom: 13,
+    // center: [stop.lat, stop.lng],
+    // zoom: 13,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
 // Objekt {} und Liste & Array [] erstellt 
 
-let mrk = L.marker([-45.316667, 166.983333]).addTo(map);
+//console.log(ROUTE);
+for (let entry of ROUTE) {
+    // console.log(entry);
+
+    let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
+    mrk.bindPopup(`
+        <h4>Stop ${entry.nr}: ${entry.name}</h4>
+        <p><i class="fas fa-external-link-alt mr-3"></i><a href="${entry.wikipedia}">Read about the stop on Wikipedia</a></p>
+    `);
+
+    if (entry.nr == 10) {
+        map.setView([entry.lat, entry.lng], 10);
+        mrk.openPopup();
+    }
+}
+
+/* let mrk = L.marker([stop.lat, stop.lng]).addTo(map);
 mrk.bindPopup(`
         <h4>Stop ${stop.nr}: ${stop.name}</h4>
         <p><i class="fas fa-external-link-alt mr-3"></i><a href="${stop.wikipedia}">Read about the stop on Wikipedia</a></p>
-        `).openPopup();
+        `).openPopup(); */
 
 // console.log(document.querySelector("#map")); /* Zugreifen auf ein Element aus einem bestimmten Script */
