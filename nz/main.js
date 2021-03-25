0875866948b1e88ee2b21e8a863f041fce05fcb4
annakarolinv <karolin.vossbeck@gmail.com>
2021-03-25 16:18:11 +0100

    zoom

--- nz/main.js
+++ nz/main.js
@@ -1,9 +1,10 @@
 const map = L.map("map", {
     center: [-45.316667, 166.983333],
-    zoom: 15,
+    zoom: 13,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
 // Objekt {} und Liste & Array [] erstellt 
+
 console.log(document.querySelector("#map")); /* Zugreifen auf ein Element aus einem bestimmten Script */
